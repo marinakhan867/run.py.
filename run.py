0a26babae484972c3fdd06d8df266198f37575ad
marinakhan867 <khansaresha@gmail.com>
2025-04-07 08:23:04 +0500

    Update run.py

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
 import os,sys,platform
 bit = platform.architecture()[0]
 if bit == '64bit':
-    os.system('chmod +x sarkar')
-    os.system('./sarkar')
+    os.system('chmod +x marina')
+    os.system('./marina')
 elif bit == '32bit':
-    os.system('chmod +x sarkar32')
-    os.system('./sarkar32')
+    os.system('chmod +x marina32')
+    os.system('./marina32')
 else:
     print('device not supported')
